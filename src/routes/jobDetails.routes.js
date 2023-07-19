b6a03f64d1b56d6114955f220db4145b9272d5e4
Muhammad Fauzan Mufti Dhana <fauzanmd5939@gmail.com>
2023-07-19 11:22:57 +0700

    add auth hit API

--- src/routes/jobDetails.routes.js
+++ src/routes/jobDetails.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const jobDetailsController = require('../controllers/jobDetails.controllers');
+const authenticate = require('../middleware/authentication');
 
-router.get('/get-job-details', jobDetailsController.getJobsDetailsList);
+router.get('/get-job-details', authenticate,jobDetailsController.getJobsDetailsList);
 module.exports = router;
--- src/routes/jobList.routes.js
+++ src/routes/jobList.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const jobListController = require('../controllers/jobList.controller');
+const authenticate = require('../middleware/authentication');
 
-router.get('/get-job-list', jobListController.getJobList);
+router.get('/get-job-list', authenticate,jobListController.getJobList);
 
 
 module.exports = router;
